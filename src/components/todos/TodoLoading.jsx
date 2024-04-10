d6a5304442a26be813fa34f810a9974cd66fb3f5
post nginx changes? <>
2024-04-10 11:13:03 +0100

    Updated loading message

--- src/components/todos/TodoLoading.jsx
+++ src/components/todos/TodoLoading.jsx
@@ -2,7 +2,7 @@ import './TodoLoading.module.css';
 
 const TodoLoading = (props) => {
   if (props.isLoading) {
-    return <p>Loading the To-do list via the API...</p>;
+    return <p>Loading the To-do list via the API... This may take a moment (freebie hosting!)</p>;
   }
 
   return <div></div>;
