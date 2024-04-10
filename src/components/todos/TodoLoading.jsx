85ee3a548a9ef62323492e6eecb245d7b6d1b166
post nginx changes? <>
2024-04-10 11:16:02 +0100

    Formatting for previous commit

--- src/components/todos/TodoLoading.jsx
+++ src/components/todos/TodoLoading.jsx
@@ -2,7 +2,9 @@ import './TodoLoading.module.css';
 
 const TodoLoading = (props) => {
   if (props.isLoading) {
-    return <p>Loading the To-do list via the API... This may take a moment (freebie hosting!)</p>;
+    return <p>Loading the To-do list via the API... 
+      <br /><br />
+      ...This may take a moment (freebie hosting!)</p>;
   }
 
   return <div></div>;
